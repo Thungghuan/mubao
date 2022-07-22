bd443671e5d116413a441013ac0628d55be99fb2
Thungghuan <1092887426@qq.com>
2022-07-22 13:21:04 +0800

    feat: add basic command

--- index.ts
+++ index.ts
@@ -3,4 +3,12 @@ import { Bot } from 'mumu-bot'
 
 const bot = new Bot(config)
 
+bot.command('show_birthday', (ctx) => {
+  ctx.reply('这个群记录的生日信息:')
+})
+
+bot.command('add_birthday', (ctx) => {
+  ctx.reply('成功添加')
+})
+
 bot.start()
